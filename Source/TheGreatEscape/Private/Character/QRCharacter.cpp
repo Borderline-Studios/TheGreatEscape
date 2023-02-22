b3cc7042c89dec08e133b5d62270416baacf44a9
GingerLandWhale <38830075+GingerLandWhale@users.noreply.github.com>
2023-02-23 10:29:24 +1300

    NewPlayerBase character working with gas
    POG

---
+++ Source/TheGreatEscape/Private/Character/QRCharacter.cpp
@@ -0,0 +1,195 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/QRCharacter.h"
+#include "Animation/AnimInstance.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/InputSettings.h"
+
+
+// Sets default values
+AQRCharacter::AQRCharacter()
+{
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+
+	// set our turn rates for input
+	TurnRateGamepad = 45.f;
+
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
+	Mesh1P->SetRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
+
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UQRAbilitySystemComponent>(TEXT("Ability System"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
+	Attributes = CreateDefaultSubobject<UQRAttributeSet>(TEXT("Attributes"));
+
+}
+
+void AQRCharacter::HandleDamage(float DamageAmount, const FHitResult& HitInfo, const FGameplayTagContainer& DamageTags,
+	ATheGreatEscapeCharacter* InstigatorCharacter, AActor* DamagerCauser)
+{
+	OnDamaged(DamageAmount, HitInfo, DamageTags, InstigatorCharacter, DamagerCauser); 
+}
+
+void AQRCharacter::HandleHealthChanged(float Deltavalue, const FGameplayTagContainer& EventTags)
+{
+	if(bAbilitiesInitalized)
+	{
+		OnHealthChanged(Deltavalue, EventTags); 
+	}	
+}
+
+void AQRCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	//Server Gas Init
+	if(AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	}
+	AddStartupGameplayAbilities();
+}
+
+void AQRCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	//Server Gas Init
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+
+	if (AbilitySystemComponent && InputComponent)
+	{
+		const FGameplayAbilityInputBinds Binds("Confirm", "Cancel", "EGASAbilityInputID",
+			static_cast<int32>(EGASAbilityInputID::Confirm),static_cast<int32>(EGASAbilityInputID::Cancel));
+		AbilitySystemComponent->BindAbilityActivationToInputComponent(InputComponent, Binds);
+	}
+}
+
+void AQRCharacter::AddStartupGameplayAbilities()
+{
+	check(AbilitySystemComponent);
+	if(GetLocalRole() == ROLE_Authority && !bAbilitiesInitalized)
+	{
+		//Grant Abilities, but only on Server
+		for(TSubclassOf<UQRGameplayAbility>& StartupAbility : GameplayAbilities)
+		{
+			AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(
+				StartupAbility, 1,
+				static_cast<int32>(StartupAbility.GetDefaultObject()->AbilityInputID), this));
+		}
+	}
+
+	for (const TSubclassOf<UGameplayEffect>& GameplayEffect : PassiveGameplayEffects)
+	{
+		FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+		EffectContext.AddSourceObject(this);
+
+		FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffect, 1, EffectContext);
+
+		if (NewHandle.IsValid())
+		{
+			FActiveGameplayEffectHandle ActiveGameplayEffectHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(
+				*NewHandle.Data.Get(), AbilitySystemComponent);
+		}
+
+		bAbilitiesInitalized = true;
+	}
+	
+}
+// Called when the game starts or when spawned
+void AQRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AQRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AQRCharacter::MoveForward(float Val)
+{
+	if (Val != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorForwardVector(), Val);
+	}
+}
+
+void AQRCharacter::MoveRight(float Val)
+{
+	if (Val != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorRightVector(), Val);
+	}
+}
+
+void AQRCharacter::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+}
+
+void AQRCharacter::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+}
+
+// Called to bind functionality to input
+void AQRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	// Set up gameplay key bindings
+	check(PlayerInputComponent);
+
+	// Bind jump events
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	// Bind movement events
+	PlayerInputComponent->BindAxis("Move Forward / Backward", this, &AQRCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Move Right / Left", this, &AQRCharacter::MoveRight);
+
+	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+	// "Mouse" versions handle devices that provide an absolute delta, such as a mouse.
+	// "Gamepad" versions are for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn Right / Left Mouse", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Look Up / Down Mouse", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn Right / Left Gamepad", this, &AQRCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("Look Up / Down Gamepad", this, &AQRCharacter::LookUpAtRate);
+
+
+	if (AbilitySystemComponent && InputComponent)
+	{
+		const FGameplayAbilityInputBinds Binds("Confirm", "Cancel", "EGASAbilityInputID",
+			static_cast<int32>(EGASAbilityInputID::Confirm),static_cast<int32>(EGASAbilityInputID::Cancel));
+		AbilitySystemComponent->BindAbilityActivationToInputComponent(InputComponent, Binds);
+	}
+}
+
+UAbilitySystemComponent* AQRCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
---
+++ Source/TheGreatEscape/Public/Character/QRCharacter.h
@@ -0,0 +1,126 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+
+#include "QRAttributeSet.h"
+#include "QRAbilitySystemComponent.h"
+#include "QRGameplayAbility.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "QRCharacter.generated.h"
+
+class UInputComponent;
+class USkeletalMeshComponent;
+class USceneComponent;
+class UCameraComponent;
+class UAnimMontage;
+class USoundBase;
+
+class UQRGameplayAbility;
+class UQRAbilitySystemComponent;
+class UQRAttributeSet;
+
+UCLASS(config=Game)
+class THEGREATESCAPE_API AQRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+	
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	USkeletalMeshComponent* Mesh1P;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FirstPersonCameraComponent;
+
+	
+
+public:
+	// Sets default values for this character's properties
+	AQRCharacter();
+	
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Abilities")
+	TArray<TSubclassOf<UGameplayEffect>> PassiveGameplayEffects;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Abilities")
+	TArray<TSubclassOf<UQRGameplayAbility>> GameplayAbilities;
+
+	UPROPERTY()
+	uint8 bAbilitiesInitalized:1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<UQRAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UQRAttributeSet> Attributes;
+
+	//Functions
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnDamaged(float DamageAmount, const FHitResult& HitInfo,
+				   const struct FGameplayTagContainer& DamageTags,
+				   ATheGreatEscapeCharacter* InstigatorCharacter, AActor* DamagerCauser);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnHealthChanged(float Deltavalue, const struct FGameplayTagContainer& EventTags);
+
+	virtual void HandleDamage(float DamageAmount, const FHitResult& HitInfo,
+				   const struct FGameplayTagContainer& DamageTags,
+				   ATheGreatEscapeCharacter* InstigatorCharacter, AActor* DamagerCauser);
+
+	virtual void HandleHealthChanged(float Deltavalue, const struct FGameplayTagContainer& EventTags);
+	
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+	void AddStartupGameplayAbilities();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float TurnRateGamepad;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Handles moving forward/backward */
+	void MoveForward(float Val);
+
+	/** Handles strafing movement, left and right */
+	void MoveRight(float Val);
+
+	/**
+	 * Called via input to turn at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to turn look up/down at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void LookUpAtRate(float Rate);
+
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	/** Returns Mesh1P subobject **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
+
+
+
+};
