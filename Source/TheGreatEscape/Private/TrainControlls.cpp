5a87907cf26fd59b1be62a951e8efc045e7fb109
Toni Natta <toninatta02@gmail.com>
2023-02-24 17:11:15 +1300

    Added train controls

---
+++ Source/TheGreatEscape/Private/TrainControlls.cpp
@@ -0,0 +1,71 @@
+// Bachelor of Software Engineering
+// Media Design School
+// Auckland
+// New Zealand
+// 
+// (c) 2022 Media Design School
+//
+// File Name   : TrainControls.h
+// Description : Updates orientation of train controls based on the train setting
+// Author      : Borderline Studios - Toni Natta
+// Mail        : toni.natta@mds.ac.nz
+
+
+#include "TrainControlls.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+ATrainControlls::ATrainControlls()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATrainControlls::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube'"));
+	
+}
+
+// Called every frame
+void ATrainControlls::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+/**
+	 * @brief Update the train control rotation based on the setting
+	 * @param ControlSetting The Setting you wish to change to
+	 */
+void ATrainControlls::UpdateControls(ETrainControlSetting ControlSetting)
+{
+	CurrentRotation = TrainControlMesh->GetRelativeRotation();
+
+	// Rotate the mesh based on the setting
+	switch (ControlSetting)
+	{
+	case ETrainControlSetting::Slow:
+		{
+			RotateValue = -50.0f;
+			TrainControlMesh->SetRelativeRotation(FMath::Lerp(FQuat(CurrentRotation), FQuat(FRotator(RotateValue, 0.0f, 0.0f)), 0.01f));
+		}
+	case ETrainControlSetting::Normal:
+		{
+			RotateValue = 0.0f;
+			TrainControlMesh->SetRelativeRotation(FMath::Lerp(FQuat(CurrentRotation), FQuat(FRotator(RotateValue, 0.0f, 0.0f)), 0.01f));
+		}
+	case ETrainControlSetting::Fast:
+		{
+			RotateValue = 50.0f;
+			TrainControlMesh->SetRelativeRotation(FMath::Lerp(FQuat(CurrentRotation), FQuat(FRotator(RotateValue, 0.0f, 0.0f)), 0.01f));
+		}
+	}
+	
+}
---
+++ Source/TheGreatEscape/Public/TrainControlls.h
@@ -0,0 +1,56 @@
+// Bachelor of Software Engineering
+// Media Design School
+// Auckland
+// New Zealand
+// 
+// (c) 2022 Media Design School
+//
+// File Name   : TrainControls.h
+// Description : Updates orientation of train controls based on the train setting
+// Author      : Borderline Studios - Toni Natta
+// Mail        : toni.natta@mds.ac.nz
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TrainControlls.generated.h"
+
+UCLASS()
+class THEGREATESCAPE_API ATrainControlls : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATrainControlls();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	enum class ETrainControlSetting
+	{
+		Slow,
+		Normal,
+		Fast
+	};
+
+	// Function to update the rotation of the train controls
+	void UpdateControls(ETrainControlSetting ControlSetting);
+
+	ETrainControlSetting ControlSetting = ETrainControlSetting::Slow;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* TrainControlMesh;
+
+
+private:
+	FRotator CurrentRotation;
+
+	float RotateValue;
+};
