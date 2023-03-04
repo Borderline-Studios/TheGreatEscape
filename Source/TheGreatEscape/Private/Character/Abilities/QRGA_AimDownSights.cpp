4612e7918a1449265c6a64b44721fefcce277045
GingerLandWhale <38830075+GingerLandWhale@users.noreply.github.com>
2023-03-04 16:28:03 +1300

    Minor changes. Nothing really of note
    ADS stuff

---
+++ Source/TheGreatEscape/Private/Character/Abilities/QRGA_AimDownSights.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Abilities/QRGA_AimDownSights.h"
+
+#include "Character/Player/PlayerCharacter.h"
+#include "Evaluation/IMovieSceneEvaluationHook.h"
+
+UQRGA_AimDownSights::UQRGA_AimDownSights()
+{
+	AbilityInputID = EGASAbilityInputID::AimDownSights;
+}
+
+void UQRGA_AimDownSights::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if(GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("ADS acti"));
+	}
+	
+	GetPlayerReferance()->Mesh1P->SetRelativeLocationAndRotation(FVector(-0.965146, -13.549654,-133.708445), FRotator(0.625515, -0.607649, -85.000542),false
+		,nullptr, ETeleportType::None);
+
+
+
+	//EndAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true, false);
+}
+
+
+
+
+void UQRGA_AimDownSights::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+
+
+
+
+bool UQRGA_AimDownSights::CanActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags,
+	const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+	return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+}
+
+APlayerCharacter* UQRGA_AimDownSights::GetPlayerReferance()
+{
+	APlayerCharacter* CharacterRef = Cast<APlayerCharacter>(GetAvatarActorFromActorInfo());
+	return CharacterRef;
+}
