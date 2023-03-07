10da689272d54e8b86124415a2a48f8dd13b3fe6
GingerLandWhale <38830075+GingerLandWhale@users.noreply.github.com>
2023-03-07 14:06:56 +1300

    Test Level made and Interactable made

---
+++ Source/TheGreatEscape/Public/Character/Abilities/QRGA_Interact.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "QRGameplayAbility.h"
+#include "QRGA_Interact.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEGREATESCAPE_API UQRGA_Interact : public UQRGameplayAbility
+{
+	GENERATED_BODY()
+
+	UQRGA_Interact();
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const override;
+	
+	APlayerCharacter* GetPlayerReferance();
+	AActor* GetPLayerActor();
+	
+};
