770fc1821069db74562f53823b731f5f86e23aac
GingerLandWhale <38830075+GingerLandWhale@users.noreply.github.com>
2023-03-10 15:41:43 +1300

    Player now has Audio again

--- Source/TheGreatEscape/Public/Character/Abilities/QRGA_Reload.h
+++ Source/TheGreatEscape/Public/Character/Abilities/QRGA_Reload.h
@@ -14,18 +14,22 @@ UCLASS()
 class THEGREATESCAPE_API UQRGA_Reload : public UQRGameplayAbility
 {
 	GENERATED_BODY()
-
+public:
 	UQRGA_Reload();
 
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const override;
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = SFX)
+	USoundBase* ReloadSFX;
+
+	
 	UFUNCTION()
 	void CallEndAbility(FName NotifyName, const FBranchingPointNotifyPayload& BranchingPointNotifyPayload);
 
 
-	APlayerCharacter* GetPlayerReferance();
+	APlayerCharacter* GetPlayerReference();
 };
 
 
